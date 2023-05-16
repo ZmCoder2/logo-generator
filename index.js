25e4a32cddcd659112cad6378e7481a0718beeb2
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-05-16 18:11:54 -0400

    left off on index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const logoGenerator = require('./utils/logoGenerator');
+
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter three characters you would like to use for your logo.',
+        name: 'characters'
+    },
+    {
+        type: 'list',
+        message: 'Select a shape from the list below that you would like to use for your logo.',
+        name: 'shapes',
+        choices: [
+            "Triangle",
+            "Circle",
+            "Square"
+        ]
+    },
+    {
+        type: 'input',
+        message: 'Enter a color you would like your text to be.',
+        name: 'textColor',
+        
+    }, 
+    {
+        type: 'input',
+        message: 'Enter a color that you would like your shape to be.',
+        name: 'shapeColor',
+    },  
+];
+
+function writeToFile(fileName, data) {
+    const logo = logoGenerator(data);
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.error(err);
+        } else {
+            console.log(`Success Logo generated!`);
+        }
+    })
+};
